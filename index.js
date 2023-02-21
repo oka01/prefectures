b5b07e6055257a2eaf0c20c07a079835f5ee7800
Okamoto <67358406+oka01@users.noreply.github.com>
2023-02-21 21:53:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import axios from 'axios'
 
-ACCESS_TOKEN = "CUyR79smOCQ19fWQWllDti0rBs15Dpcx0XkgqcAf";
-function api(path){
-  const response = axios.get("https://opendata.resas-portal.go.jp/api/v1/${path}",
-        {
-          headers: { "X-API-KEY": ACCESS_TOKEN }
-        }
-      );
-  return response;
-},
- const path = "prefectures";
-      try {
-        const response = await this.fetchAPI(path);
-        this.prefectures = response.data.result.map(val => {
-          return {
-            id: val["prefCode"],
-            name: val["prefName"],
-            isChecked: false
-          };
-        });
-      } catch (error) {
-        console.error(error.message);
+export default {
+  data() {
+    return {
+      prefectures: [],
+      selectedPrefectures: [],
+    };
+  },
+  mounted() {
+    this.fetchPrefectures();
+  },
+  methods: {
+    async fetchPrefectures() {
+      const apiKey = "CUyR79smOCQ19fWQWllDti0rBs15Dpcx0XkgqcAf";
+      const apiUrl = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+
+      const response = await fetch(apiUrl, {
+        headers: {
+          "X-API-KEY": apiKey,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
 
-
+      const data = await response.json();
+      this.prefectures = data.result;
+    },
+  },
+};
