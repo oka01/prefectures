ef58ae1a910bdadaeb63cf4f7ba970a0ecef1f9d
Okamoto <67358406+oka01@users.noreply.github.com>
2023-02-21 20:33:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+import axios from 'axios'
+
+ACCESS_TOKEN = "CUyR79smOCQ19fWQWllDti0rBs15Dpcx0XkgqcAf";
+function api(path){
+  const response = axios.get("https://opendata.resas-portal.go.jp/api/v1/${path}",
+        {
+          headers: { "X-API-KEY": ACCESS_TOKEN }
+        }
+      );
+  return response;
+},
+ const path = "prefectures";
+      try {
+        const response = await this.fetchAPI(path);
+        this.prefectures = response.data.result.map(val => {
+          return {
+            id: val["prefCode"],
+            name: val["prefName"],
+            isChecked: false
+          };
+        });
+      } catch (error) {
+        console.error(error.message);
+      }
+
 
